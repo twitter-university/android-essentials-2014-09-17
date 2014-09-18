b4dc992d40d4eb899bae4a57e64b2a263424d136
Ken Jones <kennethj@twitter.com>
2014-09-18 13:45:59 -0400

    Yamba with emulated tweet posting in AsyncTask

--- app/src/main/java/com/twitter/university/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/yamba/TweetActivity.java
@@ -1,20 +1,32 @@
 package com.twitter.university.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class TweetActivity extends Activity
         implements View.OnClickListener {
 
+    private static final String TAG = "TweetActivity";
+
+    EditText mEditTweet;
+    Toast mToast;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
 
+        mToast = Toast.makeText(this, null, Toast.LENGTH_LONG);
+        mEditTweet = (EditText) findViewById(R.id.edit_tweet);
         Button buttonTweet = (Button) findViewById(R.id.button_tweet);
         buttonTweet.setOnClickListener(this);
     }
@@ -25,9 +37,42 @@ public class TweetActivity extends Activity
         switch (id) {
             case R.id.button_tweet:
                 // Tweet button clicked
+                if (BuildConfig.DEBUG) Log.d(TAG, "Tweet button clicked");
+                String msg = mEditTweet.getText().toString();
+                if (BuildConfig.DEBUG) Log.d(TAG, "User entered: " + msg);
+                mEditTweet.setText("");
+                if (!TextUtils.isEmpty(msg)) {
+                    new PostToTwitter().execute(msg);
+                }
                 break;
             default:
                 // Unknown button clicked?!?
+        }
+    }
+
+    class PostToTwitter extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            String msg;
+            int result = R.string.tweet_post_fail;
+            if (params.length >= 1) {
+                msg = params[0];
+                // Emulate posting over the network
+                if (BuildConfig.DEBUG) Log.d(TAG, "Posting tweet: " + msg);
+                try {
+                    Thread.sleep(6000);
+                    result = R.string.tweet_post_success;
+                } catch (InterruptedException e) {
+                    Log.d(TAG, "We got interrupted", e);
+                }
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            mToast.setText(result);
+            mToast.show();
         }
     }
 
