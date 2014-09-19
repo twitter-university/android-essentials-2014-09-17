7834d26f412edf2c152e9020e229169f913b3b6c
Ken Jones <kennethj@twitter.com>
2014-09-19 13:34:28 -0400

    Refactored ActivityBar management into YambaActivity

--- app/src/main/java/com/twitter/university/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/yamba/TweetActivity.java
@@ -10,8 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
-public class TweetActivity extends Activity
+public class TweetActivity extends YambaActivity
     implements View.OnClickListener {
 
   private static final String TAG = "TweetActivity";
@@ -45,22 +44,4 @@ public class TweetActivity extends Activity
     }
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.tweet, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
 }
